9879dbfdfddffb1fdee9acabfce584ffd0c1cfc3
GuTaoZi <first_fan@outlook.com>
2022-10-16 02:48:02 +0800

    Total Project Organized

---
+++ CMakeFiles/better_calc.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/better_calc.dir/src/RPN.cpp.o"
+  "CMakeFiles/better_calc.dir/src/RPN.cpp.o.d"
+  "CMakeFiles/better_calc.dir/src/func.cpp.o"
+  "CMakeFiles/better_calc.dir/src/func.cpp.o.d"
+  "CMakeFiles/better_calc.dir/src/main.cpp.o"
+  "CMakeFiles/better_calc.dir/src/main.cpp.o.d"
+  "CMakeFiles/better_calc.dir/src/operators.cpp.o"
+  "CMakeFiles/better_calc.dir/src/operators.cpp.o.d"
+  "CMakeFiles/better_calc.dir/src/utils.cpp.o"
+  "CMakeFiles/better_calc.dir/src/utils.cpp.o.d"
+  "CMakeFiles/better_calc.dir/src/variables.cpp.o"
+  "CMakeFiles/better_calc.dir/src/variables.cpp.o.d"
+  "better_calc"
+  "better_calc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/better_calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
